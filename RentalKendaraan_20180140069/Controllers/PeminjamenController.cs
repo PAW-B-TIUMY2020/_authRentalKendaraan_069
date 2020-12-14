f1ce8339b134b1c2ee17684122415d002923c71e
Fadia Rani <fadiarani9@gmail.com>
2020-12-14 22:40:32 +0700

    Add project files.

---
+++ RentalKendaraan_20180140069/Controllers/PeminjamenController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentalKendaraan_20180140069.Models;
+
+namespace RentalKendaraan_20180140069.Controllers
+{
+    public class PeminjamenController : Controller
+    {
+        private readonly RentKendaraanContext _context;
+
+        public PeminjamenController(RentKendaraanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Peminjamen
+        public async Task<IActionResult> Index(string ktsd, string searchString, string sortOrder, string currentFilter, int? pageNumber)
+        {
+            var ktsdList = new List<string>();
+            var ktsdQuery = from d in _context.Peminjaman orderby d.IdKendaraanNavigation select d.IdKendaraanNavigation.NamaKendaraan;
+            ktsdList.AddRange(ktsdQuery.Distinct());
+
+            ViewBag.ktsd = new SelectList(ktsdList);
+            var menu = from m in _context.Peminjaman.Include(p => p.IdCustomerNavigation).Include(p => p.IdJaminanNavigation) select m;
+
+            //membuat pagelist
+            ViewData["CurrentSort"] = sortOrder;
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            //definisi jumlah data pada halaman
+            int pageSize = 5;
+            return View(await PaginatedList<Peminjaman>.CreateAsync(menu.AsNoTracking(), pageNumber ?? 1, pageSize));
+            
+        }
+
+        // GET: Peminjamen/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var peminjaman = await _context.Peminjaman
+                .Include(p => p.IdCustomerNavigation)
+                .Include(p => p.IdJaminanNavigation)
+                .Include(p => p.IdKendaraanNavigation)
+                .FirstOrDefaultAsync(m => m.IdPeminjaman == id);
+            if (peminjaman == null)
+            {
+                return NotFound();
+            }
+
+            return View(peminjaman);
+        }
+
+        // GET: Peminjamen/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCustomer"] = new SelectList(_context.Customer, "IdCostumer", "IdCostumer");
+            ViewData["IdJaminan"] = new SelectList(_context.Jaminan, "IdJaminan", "IdJaminan");
+            ViewData["IdKendaraan"] = new SelectList(_context.Kendaraan, "IdKendaraan", "IdKendaraan");
+            return View();
+        }
+
+        // POST: Peminjamen/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPeminjaman,TglPeminjaman,IdKendaraan,IdCustomer,IdJaminan,Biaya")] Peminjaman peminjaman)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(peminjaman);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCustomer"] = new SelectList(_context.Customer, "IdCostumer", "IdCostumer", peminjaman.IdCustomer);
+            ViewData["IdJaminan"] = new SelectList(_context.Jaminan, "IdJaminan", "IdJaminan", peminjaman.IdJaminan);
+            ViewData["IdKendaraan"] = new SelectList(_context.Kendaraan, "IdKendaraan", "IdKendaraan", peminjaman.IdKendaraan);
+            return View(peminjaman);
+        }
+
+        // GET: Peminjamen/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var peminjaman = await _context.Peminjaman.FindAsync(id);
+            if (peminjaman == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCustomer"] = new SelectList(_context.Customer, "IdCostumer", "IdCostumer", peminjaman.IdCustomer);
+            ViewData["IdJaminan"] = new SelectList(_context.Jaminan, "IdJaminan", "IdJaminan", peminjaman.IdJaminan);
+            ViewData["IdKendaraan"] = new SelectList(_context.Kendaraan, "IdKendaraan", "IdKendaraan", peminjaman.IdKendaraan);
+            return View(peminjaman);
+        }
+
+        // POST: Peminjamen/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPeminjaman,TglPeminjaman,IdKendaraan,IdCustomer,IdJaminan,Biaya")] Peminjaman peminjaman)
+        {
+            if (id != peminjaman.IdPeminjaman)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(peminjaman);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PeminjamanExists(peminjaman.IdPeminjaman))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCustomer"] = new SelectList(_context.Customer, "IdCostumer", "IdCostumer", peminjaman.IdCustomer);
+            ViewData["IdJaminan"] = new SelectList(_context.Jaminan, "IdJaminan", "IdJaminan", peminjaman.IdJaminan);
+            ViewData["IdKendaraan"] = new SelectList(_context.Kendaraan, "IdKendaraan", "IdKendaraan", peminjaman.IdKendaraan);
+            return View(peminjaman);
+        }
+
+        // GET: Peminjamen/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var peminjaman = await _context.Peminjaman
+                .Include(p => p.IdCustomerNavigation)
+                .Include(p => p.IdJaminanNavigation)
+                .Include(p => p.IdKendaraanNavigation)
+                .FirstOrDefaultAsync(m => m.IdPeminjaman == id);
+            if (peminjaman == null)
+            {
+                return NotFound();
+            }
+
+            return View(peminjaman);
+        }
+
+        // POST: Peminjamen/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var peminjaman = await _context.Peminjaman.FindAsync(id);
+            _context.Peminjaman.Remove(peminjaman);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PeminjamanExists(int id)
+        {
+            return _context.Peminjaman.Any(e => e.IdPeminjaman == id);
+        }
+    }
+}
---
+++ RentalKendaraan_20180140069/Models/Peminjaman.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RentalKendaraan_20180140069.Models
+{
+    public partial class Peminjaman
+    {
+        public Peminjaman()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+        [DisplayName("ID")]
+        public int IdPeminjaman { get; set; }
+        [DisplayName("Tanggal Peminjaman")]
+        public DateTime? TglPeminjaman { get; set; }
+        [DisplayName("Kendaraan")]
+        public int? IdKendaraan { get; set; }
+        [DisplayName("Costumer")]
+        public int? IdCustomer { get; set; }
+        [DisplayName("Jaminan")]
+        public int? IdJaminan { get; set; }
+        [DisplayName("Biaya")]
+        public int? Biaya { get; set; }
+        [DisplayName("Customer")]
+
+        public Customer IdCustomerNavigation { get; set; }
+        [DisplayName("Jaminan")]
+        public Jaminan IdJaminanNavigation { get; set; }
+        [DisplayName("Kendaraan")]
+        public Kendaraan IdKendaraanNavigation { get; set; }
+        public ICollection<Pengembalian> Pengembalian { get; set; }
+    }
+}
